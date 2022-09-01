173b6299a0805e73a4b586f08a00d2ade0999c89
Anish Paudel <Rpaudel379@gmail.com>
2022-09-02 00:32:42 +0530

    fixed typo on comments/Comments.tsx line 213 props

--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -210,7 +210,7 @@ const Comments: React.FC<CommentsProps> = ({
                     comment={comment}
                     onDeleteComment={onDeleteComment}
                     loadingDelete={loadingDeleteId === comment.id}
-                    userId={user.uid}
+                    userId={user?.uid}
                   />
                 ))}
               </>
